f1865d248ae6e4490f9377900278c014c57b171c
Dang Khoa Tran <richardktran.dev@gmail.com>
2022-06-14 10:34:05 +0700

    feat: test jwt token

--- src/Controller/API/SecurityController.php
+++ src/Controller/API/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\API;
 
 use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,13 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
-#[Route('/api', name: 'api_')]
+#[Route('/auth', name: 'auth_')]
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login', methods: 'POST')]
-    public function login(Request $request): JsonResponse
+    public function login(JWTTokenManagerInterface $JWTTokenManager): JsonResponse
     {
         $user = $this->getUser();
+        $token = $JWTTokenManager->create($user);
         if ($user === null) {
             return $this->json([
                 'message' => 'missing credentials',
@@ -26,6 +28,7 @@ class SecurityController extends AbstractController
         return $this->json([
             'user' => $user->getUserIdentifier(),
             'roles' => $user->getRoles(),
+            'token' => $token
         ]);
     }
 }
