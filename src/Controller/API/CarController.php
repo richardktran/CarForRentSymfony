43c6e61375e9a3d54fd589a1872709923915c710
richardktran <richardktran.dev@gmail.com>
2022-06-16 00:08:35 +0700

    feat: add transformer and show all cars

--- src/Controller/API/CarController.php
+++ src/Controller/API/CarController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller\API;
 
+use App\Entity\Car;
 use App\Repository\CarRepository;
+use App\Traits\JsonResponseTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/cars', name: 'car_')]
 class CarController extends AbstractController
 {
+    use JsonResponseTrait;
+
     private CarRepository $carRepository;
 
     public function __construct(CarRepository $carRepository)
@@ -22,6 +25,16 @@ class CarController extends AbstractController
     public function index(): JsonResponse
     {
         $cars = $this->carRepository->findAll();
-        dd($cars);
+        $result = [];
+        foreach ($cars as $car) {
+            $result[] = $car->jsonSerialize();
+        }
+        return $this->success($result);
+    }
+
+    #[Route('/{id}', name: 'detail')]
+    public function detail(Car $car): JsonResponse
+    {
+        return $this->success($car->jsonSerialize());
     }
 }
