7ec06fdcef33427be46eb0d4455a41e601aedb0f
richardktran <richardktran.dev@gmail.com>
2022-06-16 14:55:18 +0700

    feat: validation and move filter to repository

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -4,13 +4,17 @@ namespace App\Repository;
 
 use App\Entity\BaseEntity;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 class BaseRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry, string $entityClass = '')
+    protected string $alias;
+
+    public function __construct(ManagerRegistry $registry, string $entityClass = '', $alias = '')
     {
         parent::__construct($registry, $entityClass);
+        $this->alias = $alias;
     }
 
     public function save(BaseEntity $entity)
@@ -24,5 +28,44 @@ class BaseRepository extends ServiceEntityRepository
         $this->getEntityManager()->remove($entity);
 
         $this->getEntityManager()->flush();
+    }
+
+    protected function sortBy(QueryBuilder $cars, string $orderBy): QueryBuilder
+    {
+        if (empty($orderBy)) {
+            return $cars;
+        }
+        $orderBy = explode('.', $orderBy);
+        $field = $orderBy[0];
+        $order = $orderBy[1];
+        switch ($field) {
+            case 'created':
+                $cars = $cars->orderBy($this->alias . ".createdAt", $order);
+                break;
+
+            case 'price':
+                $cars = $cars->orderBy($this->alias . ".$field", $order);
+                break;
+            default:
+                break;
+        }
+        return $cars;
+    }
+
+
+    protected function filter(QueryBuilder $cars, string $field, mixed $value): QueryBuilder
+    {
+        if (empty($value)) {
+            return $cars;
+        }
+        return $cars->where($this->alias . ".$field = :$field")->setParameter($field, $value);
+    }
+
+    protected function andFilter(QueryBuilder $cars, string $field, mixed $value): QueryBuilder
+    {
+        if (empty($value)) {
+            return $cars;
+        }
+        return $cars->andWhere($this->alias . ".$field = :$field")->setParameter($field, $value);
     }
 }
---
+++ src/Request/CarRequest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Request;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class CarRequest extends BaseRequest
+{
+    #[Assert\Blank]
+    private ?string $color = '';
+
+    #[Assert\Type('string')]
+    private ?string $brand = '';
+
+    #[Assert\Type('int')]
+    private ?int $seats = 0;
+
+    #[Assert\Type('int')]
+    private ?int $limit = 10;
+
+
+    #[Assert\Choice(
+        choices: ['created', 'price'],
+    )]
+    private ?string $orderBy = 'created.asc';
+
+    /**
+     * @return string|null
+     */
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param string|null $color
+     */
+    public function setColor(?string $color): void
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    /**
+     * @param string|null $brand
+     */
+    public function setBrand(?string $brand): void
+    {
+        $this->brand = $brand;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getSeats(): ?int
+    {
+        return $this->seats;
+    }
+
+    /**
+     * @param int|null $seats
+     */
+    public function setSeats(?int $seats): void
+    {
+        $this->seats = $seats;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getLimit(): ?int
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @param int|null $limit
+     */
+    public function setLimit(?int $limit = 10): void
+    {
+        $this->limit = $limit;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOrderBy(): ?string
+    {
+        return $this->orderBy;
+    }
+
+    /**
+     * @param string|null $orderBy
+     */
+    public function setOrderBy(?string $orderBy): void
+    {
+        $this->orderBy = $orderBy;
+    }
+
+
+}
