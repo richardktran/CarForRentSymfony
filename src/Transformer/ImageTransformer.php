18a0e0b276377279214f85d697adaa8efdad0ea5
richardktran <richardktran.dev@gmail.com>
2022-06-17 18:02:19 +0700

    refactor: custom transformer

--- src/Transformer/ImageTransformer.php
+++ src/Transformer/ImageTransformer.php
@@ -4,13 +4,12 @@ namespace App\Transformer;
 
 use App\Entity\Image;
 
-class ImageTransformer
+class ImageTransformer extends BaseTransformer
 {
+    const ALLOW = ['id', 'path'];
+
     public function toArray(Image $image): array
     {
-        return [
-            'id' => $image->getId(),
-            'path' => $image->getPath()
-        ];
+        return $this->transform($image, static::ALLOW);
     }
 }
