0a87c5d016528bc48db8198bcf2c6f5d52fc1211
richardktran <richardktran.dev@gmail.com>
2022-06-20 13:04:17 +0700

    feat: add car without upload image

--- src/Service/CarService.php
+++ src/Service/CarService.php
@@ -2,20 +2,32 @@
 
 namespace App\Service;
 
+use App\Entity\Car;
 use App\Repository\CarRepository;
+use App\Request\AddCarRequest;
 use App\Request\CarRequest;
+use App\Transformer\CarTransformer;
 
 class CarService
 {
     private CarRepository $carRepository;
+    private CarTransformer $carTransformer;
 
-    public function __construct(CarRepository $carRepository)
+    public function __construct(CarRepository $carRepository, CarTransformer $carTransformer)
     {
         $this->carRepository = $carRepository;
+        $this->carTransformer = $carTransformer;
     }
 
     public function findAll(CarRequest $carRequest): array
     {
         return $this->carRepository->all($carRequest);
+    }
+
+    public function add(AddCarRequest $carRequest): Car
+    {
+        $car = $this->carTransformer->requestToEntity($carRequest);
+        $this->carRepository->save($car);
+        return $car;
     }
 }
