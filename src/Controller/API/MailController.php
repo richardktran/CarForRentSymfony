c0ffee492a2d9103d987a8d884f6bfe1aeae2353
richardktran <richardktran.dev@gmail.com>
2022-06-24 17:24:31 +0700

    feat: send email

---
+++ src/Controller/API/MailController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Service\MailerService;
+use App\Traits\JsonResponseTrait;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailController extends AbstractController
+{
+    use JsonResponseTrait;
+
+    #[Route('/mail/send', name: 'send_mail', methods: ['POST'])]
+    public function index(MailerService $mailerService): JsonResponse
+    {
+        $customerMail = "richardktran.dev@gmail.com";
+        $customerName = "Richard K Tran";
+        $mailerService->send($customerMail, $customerName);
+        return $this->success(['message' => 'Send mail success']);
+    }
+}
---
+++ src/Service/MailerService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class MailerService
+{
+    private array $emailConfig;
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->emailConfig = $parameterBag->get('emailConfig');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function send(string $customerEmail, string $customerName): void
+    {
+        $smtp = $this->emailConfig['smtp'];
+        $mail = new PHPMailer(true);
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = $smtp['host'];
+        $mail->SMTPAuth = true;
+        $mail->Username = $this->emailConfig['username'];
+        $mail->Password = $this->emailConfig['password'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = $smtp['port'];
+
+        $mail->setFrom($this->emailConfig['username'], $this->emailConfig['name']);
+        $mail->addAddress($customerEmail, $customerName);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Here is the subject';
+        $mail->Body = 'This is the HTML message body <b>in bold! </b>  with code:' . uniqid();
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+    }
+}
